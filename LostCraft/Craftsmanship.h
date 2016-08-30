0172187ee69364ae8d8d43777ec31833fba27ef4
高超 <gaochao@GaochaodeMacBook-Air.local>
2016-08-30 10:20:09 +0800

    modify Class of Craftsmanship

--- LostCraft/Craftsmanship.h
+++ LostCraft/Craftsmanship.h
@@ -1,4 +1,11 @@
 #pragma once
+
+const string craftnames[NUM_CRAFTS]={"普通击打","寒冰掌","石破惊雷","赤火焰烧","月蚀镰刀","圣光冲击","天师灵助","天威庇佑"};
+const int availables[NUM_CRAFTS]={1,1,5,10,20,30,35,45};
+const int mps[NUM_CRAFTS] = {0,5,10,20,40,60,80,100};
+const int atks[NUM_CRAFTS] = {10,12,15,18,20,22,25,30};
+const int coolings[NUM_CRAFTS] = {0,1,3,3,4,4,5,5};
+
 class Craftsmanship
 {
 public:
